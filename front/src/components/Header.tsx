63c813c3455ff2732029d7b26cc86324da22bee0
janlopezdelaosa <janlopezdelaosa@gmail.com>
2021-02-13 17:02:35 +0100

    Continue CD test

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header: React.FC<HeaderProps> = () => {
   return (
     <div className="p-4 md:px-20 flex justify-between items-center">
       <h2 className="font-extrabold uppercase lg:text-2xl">
-        Weather Forecaster y tal
+        Weather Forecaster y talsss
       </h2>
       <p className="text-xs lg:text-lg">
         {dayjs().format("MMMM D, YYYY | hh:mm")}

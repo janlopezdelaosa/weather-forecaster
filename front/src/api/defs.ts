d3daa71f567612bacb6402aefa119ed82c68b89f
janlopezdelaosa <janlopezdelaosa@gmail.com>
2021-02-13 15:11:12 +0100

    Prepare root for Heroku deployment

--- front/src/api/defs.ts
+++ front/src/api/defs.ts
@@ -1,4 +1,5 @@
-export const apiBase = "http://localhost:3000";
+// export const apiBase = "http://localhost:3000";
+export const apiBase = "https://morning-falls-52071.herokuapp.com";
 
 export interface LocationData {
   country: string;
